8a8184095e267bf971ec8b8051d2c1e825263515
HotView <864773190@qq.com>
2018-11-08 14:49:34 +0800

    this is book file. i want to share the book with others

---
+++ test/a.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char const *argv[]) {
+        printf("world");
+        return 0;
+}
+
---
+++ test/a.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+a = np.numarray([0, 1, 2, 3, 4, 5, 6])
+print(a)
